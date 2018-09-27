280f0854fe347921fce77ad3138f84d400ebacd9
Tyler <tgalske@gmail.com>
2018-09-27 11:59:40 -0400

    Scala updates with output txt

--- Scala/src/Main.scala
+++ Scala/src/Main.scala
@@ -1,11 +1,13 @@
+// Tyler Galske
 object Main extends  App{
   var MIN_CHAR = 65
   var MAX_CHAR = 90
-  var strIn = "This is a test string from Alan"
-  var numShifts = 8
+  var strIn = "the quick brown fox"
+  var numShifts = 20
   encrypt(strIn, numShifts)
 
-  strIn = "BPQA QA I BMAB ABZQVO NZWU ITIV"
+  strIn = "u tabq ftq kmzwqqe iuz fazustf"
+  numShifts = 12
   decrypt(strIn, numShifts)
 
   strIn = "hal"
@@ -14,6 +16,9 @@ object Main extends  App{
 
   def encrypt (strIn:String, numShifts:Int) : Unit = {
     var letters = strIn.toUpperCase.toCharArray
+    println("ENCRYPT")
+    println(letters.mkString)
+
     var i = 0
     while (i < letters.length()) {
       var j = letters(i).toInt
@@ -28,6 +33,9 @@ object Main extends  App{
 
   def decrypt (strIn:String, numShifts:Int) : Unit = {
     var letters = strIn.toUpperCase.toCharArray
+    println("\nDECRYPT")
+    println(letters.mkString)
+
     var i = 0
     while (i < letters.length()) {
       var j = letters(i).toInt
@@ -43,6 +51,9 @@ object Main extends  App{
   def solve (strIn:String, maxShiftValue:Int) : Unit = {
     var shiftValue = maxShiftValue
     val letters = strIn.toUpperCase.toCharArray
+    println("\nSOLVE")
+    println(letters.mkString)
+
     var i = 0
 
     while (shiftValue > -1) {
